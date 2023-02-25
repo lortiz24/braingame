c93ef7fa9ab1f4b658176c98154e08a22c5f9217
lortiz2022 <107954011+lortiz2022@users.noreply.github.com>
2023-02-25 13:08:08 -0500

    Se elimina importaciones no usadas

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -4,29 +4,9 @@ import { Card, Layout } from 'antd'
 import './MainLayout.css'
 
 
-const { Header, Footer, Sider, Content } = Layout;
-const headerStyle: React.CSSProperties = {
-    textAlign: 'center',
-    color: '#fff',
-    height: 64,
-    paddingInline: 50,
-    lineHeight: '64px',
-    backgroundColor: '#7dbcea',
-};
+const { Content } = Layout;
 
-const contentStyle: React.CSSProperties = {
-    textAlign: 'center',
-    minHeight: '80%',
-    lineHeight: '120px',
-    color: '#fff',
-    backgroundColor: '#108ee9',
-};
 
-const footerStyle: React.CSSProperties = {
-    textAlign: 'center',
-    color: '#fff',
-    backgroundColor: '#7dbcea',
-};
 const MainLayout: React.FC<HomePageProps> = ({ children }) => {
     return (
         <>
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
-import MainLayout from './layouts/MainLayout';
 import { AppRouter } from './routes/AppRouter';
 import './index.css'
 import { BrainGameProvider } from './context/BrainGameProvider';
